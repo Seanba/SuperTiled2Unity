74da946854e7a78716d94a0153cefddcedcdb610
Seanba <sean@seanba.com>
2019-07-20 17:06:43 -0500

    Deploying version 1.5.2

--- SuperTiled2Unity/Assets/SuperTiled2Unity/Scripts/Editor/Postprocessors/SuperTexturePostprocessor.cs
+++ SuperTiled2Unity/Assets/SuperTiled2Unity/Scripts/Editor/Postprocessors/SuperTexturePostprocessor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 namespace SuperTiled2Unity.Editor
