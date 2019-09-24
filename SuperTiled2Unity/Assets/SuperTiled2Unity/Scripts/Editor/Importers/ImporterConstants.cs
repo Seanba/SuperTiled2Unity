dce4e451348d0909a603b4d13b99cfb1a5f47230
SeanB-22ct <seanb@22ndtoys.com>
2019-09-24 13:52:24 -0500

    Deploying version 1.6.1

--- SuperTiled2Unity/Assets/SuperTiled2Unity/Scripts/Editor/Importers/ImporterConstants.cs
+++ SuperTiled2Unity/Assets/SuperTiled2Unity/Scripts/Editor/Importers/ImporterConstants.cs
@@ -4,7 +4,7 @@
     {
         public const int TilesetVersion = 10;
         public const int TemplateVersion = 4;
-        public const int MapVersion = 16;
+        public const int MapVersion = 17;
 
         public const string TilesetExtension = "tsx";
         public const string TemplateExtension = "tx";
