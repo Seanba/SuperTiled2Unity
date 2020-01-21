03b87d96803e25cbde121dd717becc25e7c4e080
Israel Jacquez <mrkotfw@gmail.com>
2020-01-21 13:03:57 -0800

    Add "unity:" prefix

--- SuperTiled2Unity/Assets/SuperTiled2Unity/Scripts/Editor/StringConstants.cs
+++ SuperTiled2Unity/Assets/SuperTiled2Unity/Scripts/Editor/StringConstants.cs
@@ -2,13 +2,14 @@
 {
     public static class StringConstants
     {
-        public const string Unity_Ignore = "unity:ignore";
-        public const string Unity_IsTrigger = "unity:IsTrigger";
-        public const string Unity_Layer = "unity:layer";
-        public const string Unity_SortingLayer = "unity:SortingLayer";
-        public const string Unity_SortingLayerName = "unity:SortingLayerName";
-        public const string Unity_SortingOrder = "unity:SortingOrder";
-        public const string Unity_Tag = "unity:tag";
-        public const string Unity_ZPosition = "unity:z-pos";
+        public const string Unity_Prefix = "unity:";
+        public const string Unity_Ignore = Unity_Prefix + "ignore";
+        public const string Unity_IsTrigger = Unity_Prefix + "IsTrigger";
+        public const string Unity_Layer = Unity_Prefix + "layer";
+        public const string Unity_SortingLayer = Unity_Prefix + "SortingLayer";
+        public const string Unity_SortingLayerName = Unity_Prefix + "SortingLayerName";
+        public const string Unity_SortingOrder = Unity_Prefix + "SortingOrder";
+        public const string Unity_Tag = Unity_Prefix + "tag";
+        public const string Unity_ZPosition = Unity_Prefix + "z-pos";
     }
 }
