7b814da78fd1480e937e81d9a0a432d25795eed5
Seanba <sean@seanba.com>
2022-07-10 18:28:24 -0500

    ST2U now supports Tiled 1.9

--- SuperTiled2Unity/Assets/SuperTiled2Unity/Scripts/Editor/Importers/ImporterConstants.cs
+++ SuperTiled2Unity/Assets/SuperTiled2Unity/Scripts/Editor/Importers/ImporterConstants.cs
@@ -2,9 +2,9 @@
 {
     public static class ImporterConstants
     {
-        public const int TilesetVersion = 12;
-        public const int TemplateVersion = 5;
-        public const int MapVersion = 25;
+        public const int TilesetVersion = 13;
+        public const int TemplateVersion = 6;
+        public const int MapVersion = 26;
         public const int WorldVersion = 1;
 
         public const string TilesetExtension = "tsx";
