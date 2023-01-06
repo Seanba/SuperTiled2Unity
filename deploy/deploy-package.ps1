f20c0f8f2fecfda43f856be7bb1865745d25b1c9
Seanba <sean@seanba.com>
2023-01-05 23:01:04 -0600

    Deploy script is working

--- deploy/deploy-package.ps1
+++ deploy/deploy-package.ps1
@@ -3,9 +3,24 @@
 try
 {
     Push-Location $PSScriptRoot
-    
-    Write-Host Packaging Super Tiled2Unity
 
+    # Get the current version of Super Tiled2Unity
+    $package_dir = '../SuperTiled2Unity/Packages/com.seanba.super-tiled2unity'
+    $package_json = "$package_dir/package.json"
+    $version = (Get-Content $package_json | ConvertFrom-Json).version
+    $output = "super-tiled2unity.v$version.zip"
+
+    Write-Host Packaging Super Tiled2Unity version $version
+    Write-Host $output
+
+    $compress = @{
+        Path = $package_dir
+        CompressionLevel = "Fastest"
+        DestinationPath = $output
+    }
+
+    Compress-Archive @compress -Force
+    Write-Host "Done compressing '$output'"
 }
 finally
 {
