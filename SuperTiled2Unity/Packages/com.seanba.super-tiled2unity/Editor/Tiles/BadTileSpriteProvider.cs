aea0a0a39b6f7b995ee52ac30ff5135beeef8657
Seanba <sean@seanba.com>
2024-03-03 00:26:35 -0600

    Adding lots of "bad tile" patterns

---
+++ SuperTiled2Unity/Packages/com.seanba.super-tiled2unity/Editor/Tiles/BadTileSpriteProvider.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace SuperTiled2Unity.Editor
+{
+    public class BadTileSpriteProvider : ScriptableSingleton<BadTileSpriteProvider>
+    {
+        public BadTileTextures m_BadTileTextures;
+
+        private static readonly Color32 m_BadColor = NamedColors.HotPink;
+        private static readonly Dictionary<string, int> m_NamesToHash = new Dictionary<string, int>();
+
+        internal bool CreateSpriteAndTile(int tileId, int width, int height, SuperTileset superTileset, out Sprite sprite, out SuperBadTile tile)
+        {
+            Assert.IsNotNull(m_BadTileTextures);
+            Assert.IsTrue(m_BadTileTextures.m_Textures.Length > 0);
+
+            // fixit - base determinstic assignment off of superTileset.name too
+            int hash = GetTilesetHash(superTileset.name);
+            var badTileTexture = m_BadTileTextures.m_Textures[(tileId + hash) % m_BadTileTextures.m_Textures.Length];
+
+            var spriteRect = Rect.MinMaxRect(0, 0, Mathf.Min(width, badTileTexture.width), Mathf.Min(height, badTileTexture.height));
+            sprite = Sprite.Create(badTileTexture, spriteRect, Vector2.zero, superTileset.m_PixelsPerUnit);
+            sprite.name = $"error-sprite-{tileId}";
+
+            tile = ScriptableObject.CreateInstance<SuperBadTile>();
+            tile.m_TileId = tileId;
+            tile.name = $"error-tile-{tileId}";
+            tile.m_Sprite = sprite;
+            tile.m_Width = width;
+            tile.m_Height = height;
+            tile.m_TileOffsetX = superTileset.m_TileOffset.x;
+            tile.m_TileOffsetY = superTileset.m_TileOffset.y;
+            tile.m_ObjectAlignment = superTileset.m_ObjectAlignment;
+            tile.m_TileRenderSize = superTileset.m_TileRenderSize;
+            tile.m_FillMode = superTileset.m_FillMode;
+            tile.m_Color = m_BadColor;
+
+            return true;
+        }
+
+        private static int GetTilesetHash(string name)
+        {
+            if (m_NamesToHash.TryGetValue(name, out int hash))
+            {
+                return hash;
+            }
+
+            // C# strings do not generate a deterministic hash code
+            hash = Mathf.Abs(Hash128.Compute(name).GetHashCode());
+            m_NamesToHash.Add(name, hash);
+            Debug.Log($"hash ({name}) = {hash}");
+            return hash;
+        }
+    }
+}
---
+++ SuperTiled2Unity/Packages/com.seanba.super-tiled2unity/Editor/Tiles/BadTileTextures.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+// Only make one of these during development
+//[CreateAssetMenu(fileName = "BadTileTextures", menuName = "Super Tiled2Unity/Create BadTileTexture files")]
+public class BadTileTextures : ScriptableObject
+{
+    public Texture2D[] m_Textures;
+}
