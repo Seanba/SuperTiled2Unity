3c7f77ac4afd811a736a3260c1542ad0966bf913
Seanba <sean@seanba.com>
2024-03-09 11:02:54 -0600

    Error reporting for missing sprites and dependency assets looking good now. (WIP)

--- SuperTiled2Unity/Packages/com.seanba.super-tiled2unity/Editor/Errors/ImportErrorsEditor.cs
+++ SuperTiled2Unity/Packages/com.seanba.super-tiled2unity/Editor/Errors/ImportErrorsEditor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using UnityEditor;
@@ -19,17 +20,29 @@ namespace SuperTiled2Unity.Editor
             {
                 ui.HelpBox("Errors Detected. Your Tiled asset may not look or function correctly. Please follow directions to fix.");
 
-                DisplayDependencyErrors(importErrors); // fixit -come back and finish this
+                DisplayDependencyErrors(ui, importErrors);
                 DisplayMissingSprites(ui, importErrors);
             }
         }
 
-        private static void DisplayDependencyErrors(ImportErrors importErrors)
+        private static void DisplayDependencyErrors(MessageBuilderUI ui, ImportErrors importErrors)
         {
             if (importErrors.m_ErrorsInAssetDependencies.Count > 0)
             {
-                EditorGUILayout.LabelField("Errors in asset dependencies", EditorStyles.boldLabel);
-                EditorGUILayout.HelpBox("fixit - dependency errors!", MessageType.Error);
+                ui.BoldLabel("Dependency Errors - Inspect Assets For Further Details");
+                foreach (var assetPath in importErrors.m_ErrorsInAssetDependencies)
+                {
+                    ui.HelpBox($"Errors found in file '{assetPath}'\nInspect asset for details.");
+
+                    using (new GuiScopedBackgroundColor(NamedColors.LightPink))
+                    {
+                        var assetName = Path.GetFileName(assetPath);
+                        if (GUILayout.Button($"Inspect '{assetName}'"))
+                        {
+                            Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(assetPath);
+                        }
+                    }
+                }
             }
         }
 
@@ -41,7 +54,38 @@ namespace SuperTiled2Unity.Editor
                 ui.BoldLabel("Missing Sprites - Reimporting Textures May Fix");
                 foreach (var missing in importErrors.m_MissingTileSprites)
                 {
-                    ui.HelpBox("The following texture is missing sprites that are needed for Super Tiled2Unity.", missing.m_TextureAssetPath);
+                    StringBuilder msg = new StringBuilder(1024 * 4);
+                    msg.AppendLine("The following texture is missing sprites that are needed for Super Tiled2Unity.");
+                    msg.AppendLine(missing.m_TextureAssetPath);
+
+                    int reportCount = 5;
+                    foreach (var tile in missing.m_MissingTiles.Take(reportCount))
+                    {
+                        msg.AppendLine($"Missing tile {tile.m_SpriteId}: ({tile.m_Rect.x}, {tile.m_Rect.y}) ({tile.m_Rect.width}x{tile.m_Rect.height})");
+                    }
+
+                    if (missing.m_MissingTiles.Count > reportCount)
+                    {
+                        msg.AppendLine($"An additional {missing.m_MissingTiles.Count - reportCount} sprites are missing. Total = {missing.m_MissingTiles.Count}");
+                    }
+
+                    msg.AppendLine("Super Tiled2Unity will attempt to automatically add these missing sprites on import. Try reimporting.");
+
+                    ui.HelpBox(msg.ToString());
+
+                    using (new GuiScopedBackgroundColor(NamedColors.LightPink))
+                    {
+                        var assetName = Path.GetFileName(missing.m_TextureAssetPath);
+                        if (GUILayout.Button($"Inspect '{assetName}'"))
+                        {
+                            Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(missing.m_TextureAssetPath);
+                        }
+
+                        if (GUILayout.Button($"Reimport '{assetName}'"))
+                        {
+                            AssetDatabase.ImportAsset(missing.m_TextureAssetPath); // fixit - SuperTexturePostprocessor not working correctly yet
+                        }
+                    }
                 }
             }
         }
@@ -50,11 +94,6 @@ namespace SuperTiled2Unity.Editor
         {
             private GuiScopedBackgroundColor m_Background = new GuiScopedBackgroundColor(Color.red);
             private StringBuilder m_StringBuilder = new StringBuilder(1024 * 8);
-
-            public void HelpBox(params string[] msgs)
-            {
-                HelpBox(string.Join("\n", msgs));
-            }
 
             public void HelpBox(string msg)
             {
