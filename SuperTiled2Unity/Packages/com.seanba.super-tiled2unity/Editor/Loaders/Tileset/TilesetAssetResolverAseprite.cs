6150fe09625f87aa5ad1d93a1c36d8c1e5371400
Seanba <sean@seanba.com>
2025-03-05 23:35:53 -0600

    wip animation clip

--- SuperTiled2Unity/Packages/com.seanba.super-tiled2unity/Editor/Loaders/Tileset/TilesetAssetResolverAseprite.cs
+++ SuperTiled2Unity/Packages/com.seanba.super-tiled2unity/Editor/Loaders/Tileset/TilesetAssetResolverAseprite.cs
@@ -14,7 +14,7 @@ namespace SuperTiled2Unity.Editor
         private AnimationClip m_AseAnimationClip;
         private List<Sprite> m_AseSprites;
 
-        private bool m_AreImportSettingsValid;
+        private bool m_WasSuccessfullyImported;
 
         public TilesetAssetResolverAseprite(string sourceAssetPath, TiledAssetImporter tiledAssetImporter, SuperTileset superTileset, AsepriteImporter asepriteImporter)
             : base(sourceAssetPath, tiledAssetImporter, superTileset)
@@ -24,60 +24,111 @@ namespace SuperTiled2Unity.Editor
 
         public override bool AddSpritesAndTile(int tileId, int srcx, int srcy, int tileWidth, int tileHeight)
         {
-            if (!m_AreImportSettingsValid)
+            if (!m_WasSuccessfullyImported)
             {
                 return false;
             }
 
-            // fixit - what to do
+            if (m_AseTexture == null)
+            {
+                return false;
+            }
+
+            if (m_AseSprites?.Any() != true)
+            {
+                return false;
+            }
+
+            if (m_AseAnimationClip == null)
+            {
+                return false;
+            }
+
+            // fixit - add sprites for each frame
+
+            // fixit - add the tile
+            // fixit - The tile must have animation sprites (created just above)
 
             return false;
         }
 
         protected override void OnPrepare()
         {
+            m_WasSuccessfullyImported = true;
+
             if (AsepriteImporter.importMode != FileImportModes.AnimatedSprite)
             {
-                m_AreImportSettingsValid = false;
+                m_WasSuccessfullyImported = false;
                 TiledAssetImporter.ReportErrorsInDependency(SourceAssetPath, "File import mode must be Animated Sprite");
-                return;
             }
 
             if (AsepriteImporter.layerImportMode != LayerImportModes.MergeFrame)
             {
-                m_AreImportSettingsValid = false;
+                m_WasSuccessfullyImported = false;
                 TiledAssetImporter.ReportErrorsInDependency(SourceAssetPath, "Layer import mode must be Merge Frame");
-                return;
             }
 
             if (AsepriteImporter.generateAnimationClips == false)
             {
-                m_AreImportSettingsValid = false;
+                m_WasSuccessfullyImported = false;
                 TiledAssetImporter.ReportErrorsInDependency(SourceAssetPath, "Must generate Animation Clips");
-                return;
             }
 
             if (AsepriteImporter.spritePadding != 0)
             {
-                m_AreImportSettingsValid = false;
+                m_WasSuccessfullyImported = false;
                 TiledAssetImporter.ReportErrorsInDependency(SourceAssetPath, "Must have Sprite Padding of 0");
-                return;
             }
 
             var allObjects = AssetDatabase.LoadAllAssetsAtPath(SourceAssetPath);
 
             // There should only be one texture (acting as an atlas of all the animation frames)
             m_AseTexture = allObjects.OfType<Texture2D>().FirstOrDefault();
+            if (m_AseTexture == null)
+            {
+                TiledAssetImporter.ReportErrorsInDependency(SourceAssetPath, "Could not load Texture2D");
+            }
 
             // Each sprite is a frame into the texture
             m_AseSprites = allObjects.OfType<Sprite>().ToList();
+            if (!m_AseSprites.Any())
+            {
+                TiledAssetImporter.ReportErrorsInDependency(SourceAssetPath, "Could not load any Sprites");
+            }
 
-            // fixit - every sprite must be big enough
-
-            // There should only be one animation clip. This is how we now which frames are visible when and for how long.
-            m_AseAnimationClip = allObjects.OfType<AnimationClip>().FirstOrDefault();
-
-            m_AreImportSettingsValid = true;
+            // There should only be one animation clip. This is how we know which frames are visible when and for how long.
+            m_AseAnimationClip = allObjects.OfType<AnimationClip>().FirstOrDefault(); // fixit - does this need to be a data member?
+            if (m_AseAnimationClip == null)
+            {
+                TiledAssetImporter.ReportErrorsInDependency(SourceAssetPath, "Could not load Animation Clip");
+            }
+            else
+            {
+                // fixit - every sprite must be big enough (check for that here?)
+                var bindings = AnimationUtility.GetObjectReferenceCurveBindings(m_AseAnimationClip);
+                if (bindings?.Any() != true)
+                {
+                    m_WasSuccessfullyImported = false;
+                    TiledAssetImporter.ReportErrorsInDependency(SourceAssetPath, "Could not find animation curve bindings.");
+                }
+                else
+                {
+                    var keys = AnimationUtility.GetObjectReferenceCurve(m_AseAnimationClip, bindings[0]);
+                    if (keys?.Any() != true)
+                    {
+                        m_WasSuccessfullyImported = false;
+                        TiledAssetImporter.ReportErrorsInDependency(SourceAssetPath, "Could not find animation curve keys.");
+                    }
+                    else
+                    {
+                        // Finally have the animation data we need
+                        // 1 / m_AseAnimationClip.frameRate = the time of the final frame, I think // fixit
+                        //keys[0].time  // absolute start time
+                        //keys[1].value // Sprite type expeted
+                        Debug.LogError($"fixit - found keys: {keys.Length}");
+                    }
+                }
+            }
         }
     }
 }
