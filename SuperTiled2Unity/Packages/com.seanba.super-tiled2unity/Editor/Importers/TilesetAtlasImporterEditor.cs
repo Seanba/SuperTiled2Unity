b8e8f34dc6c4b5a6ef8897a64ab1217a3f736482
seanb-22ct <seanb@22ndtoys.com>
2025-06-24 18:54:09 -0500

    Update TilesetAtlasImporterEditor.cs

--- SuperTiled2Unity/Packages/com.seanba.super-tiled2unity/Editor/Importers/TilesetAtlasImporterEditor.cs
+++ SuperTiled2Unity/Packages/com.seanba.super-tiled2unity/Editor/Importers/TilesetAtlasImporterEditor.cs
@@ -9,6 +9,7 @@ namespace SuperTiled2Unity.Editor
     {
         public override void OnInspectorGUI()
         {
+            // fixit - show an error if our Unity version does not support Sprite Atlas Packing V2 properly (before 2021.3) if we are using a V2 sprite atlas
             base.OnInspectorGUI();
         }
     }
