c5ef2b77176832d54253b8fb7450d65c9bb4204f
seanb-22ct <seanb@22ndtoys.com>
2025-06-24 18:45:17 -0500

    Add custom editor for TilesetAtlasImporter

---
+++ SuperTiled2Unity/Packages/com.seanba.super-tiled2unity/Editor/Importers/TilesetAtlasImporterEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEditor.AssetImporters;
+
+namespace SuperTiled2Unity.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(TilesetAtlasImporter))]
+    public class TilesetAtlasImporterEditor : ScriptedImporterEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+}
