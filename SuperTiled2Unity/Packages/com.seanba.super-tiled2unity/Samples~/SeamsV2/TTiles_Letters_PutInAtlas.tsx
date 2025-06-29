4ddcb5d38e2083476c165fc4ac5d36b05517321d
Seanba <sean@seanba.com>
2025-06-29 13:20:46 -0500

    Updating V2 sample (and moving/renaming V1 sample)

---
+++ SuperTiled2Unity/Packages/com.seanba.super-tiled2unity/Samples~/SeamsV2/TTiles_Letters_PutInAtlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Light_Letters (In Atlas)" tilewidth="8" tileheight="8" spacing="1" margin="1" tilecount="55" columns="11">
+ <image source="T_Letters.png" width="100" height="46"/>
+</tileset>
